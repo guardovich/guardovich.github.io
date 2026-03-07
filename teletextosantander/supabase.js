28e2908dee392ac48f33354dbd67d94ff41e4707
David Guardo <davidguardopuertas@gmail.com>
2026-03-07 19:54:19 +0100

    Update supabase.js

--- teletextosantander/supabase.js
+++ teletextosantander/supabase.js
@@ -1,1 +1,17 @@
+// teletextosantander/supabase.js
 
+const SUPABASE_URL = "https://ctzrfpnlxlkbjwgriozb.supabase.co";
+const SUPABASE_KEY = "AQUI_PEGA_TU_SUPABASE_CLIENT_API_KEY";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+
+// asegura sesión anónima para poder insertar con RLS
+async function ensureSession() {
+  const { data } = await supabaseClient.auth.getSession();
+  if (!data.session) {
+    await supabaseClient.auth.signInAnonymously();
+  }
+}
